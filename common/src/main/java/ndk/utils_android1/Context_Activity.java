003533d98a583ffcf95de73ee61f83878116fb09
Banee Ishaque K <baneeishaque@gmail.com>
2019-04-16 00:09:34 +0530

    Initial commit

---
+++ common/src/main/java/ndk/utils_android1/Context_Activity.java
@@ -0,0 +1,8 @@
+package ndk.utils_android1;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public class Context_Activity extends AppCompatActivity {
+    Context activity_context = this;
+}
---
+++ common/src/main/java/ndk/utils_android1/SharedPreferences_Activity.java
@@ -0,0 +1,14 @@
+package ndk.utils_android1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SharedPreferences_Activity extends AppCompatActivity {
+
+    public SharedPreferences get_shared_preferences() {
+        return getApplicationContext().getSharedPreferences(configure_APPLICATION_NAME(), Context.MODE_PRIVATE);
+    }
+
+    protected abstract String configure_APPLICATION_NAME();
+}
